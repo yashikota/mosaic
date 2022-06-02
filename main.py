28838fc5dfd0239256bb2ec853b64a586ce43d46
kota <52403688+yashikota@users.noreply.github.com>
2022-06-02 17:29:50 +0900

    update

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 import sys
 from tqdm import tqdm
+from typing import Tuple
 
 
-def read_image(path: str) -> tuple[np.ndarray, int, int]:
+def read_image(path: str) -> Tuple[np.ndarray, int, int]:
     # 画像を読み込む
     img: np.ndarray = cv2.imread(path)
 
     # 画像のサイズを取得
-    height: int
-    width: int
-    height, width = img.shape[:2]
+    height: int = img.shape[0]
+    width: int = img.shape[1]
 
     return img, height, width
 
 
-def get_tile_size() -> tuple[int, int]:
+def get_tile_size() -> Tuple[int, int]:
     # タイルのサイズを入力
     print("タイルのサイズを入力してください")
     x: int = int(input("縦:"))
@@ -26,8 +26,12 @@ def get_tile_size() -> tuple[int, int]:
 
 
 def compress_image() -> np.ndarray:
+    # 初期化
+    pixel_list: list = []
+    mean_list: list = []
+
     # 画像の読み込み
-    img : np.ndarray
+    img: np.ndarray
     height: int
     width: int
     img, height, width = read_image(sys.argv[1])
@@ -37,7 +41,22 @@ def compress_image() -> np.ndarray:
     y: int
     x, y = get_tile_size()
 
-    return tile
+    print("height:{}\nwidth:{}\nx:{}\ny{}".format(height, width, x, y))
+    print("height // x:{}\nwidth // y:{}".format(height // x, width // y))
+
+    # 割り切れる部分のタイル化
+    for i in tqdm((range(width // y))):
+        for j in range((height // x)):
+            # 各画素を取得
+            pixel_list.append(img[i * y : i * y + y, j * x : j * x + x].reshape(-1))
+            # 各画素の平均値を計算
+            mean_list.append(np.mean(img[i * y : i * y + y, j * x : j * x + x]))
+            # 計算した平均値を画像に反映
+            img[i * y : i * y + y, j * x : j * x + x] = np.mean(
+                img[i * y : i * y + y, j * x : j * x + x]
+            )
+
+    return img
 
 
 def output_image(img: np.ndarray) -> None:
@@ -51,10 +70,10 @@ def output_image(img: np.ndarray) -> None:
 
 def main():
     # 画像をタイル圧縮
-    tile: np.ndarray = compress_image()
+    img: np.ndarray = compress_image()
 
     # 画像を出力
-    # output_image(tile)
+    output_image(img)
 
 
 if __name__ == "__main__":
