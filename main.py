f408c7493556e36babb58c494ef33e95ad2781fc
kota <52403688+yashikota@users.noreply.github.com>
2022-06-03 01:14:40 +0900

    slim

--- main.py
+++ main.py
@@ -44,11 +44,8 @@ def monochrome_or_color() -> bool:
 
     return False
 
-def mosaic_image() -> np.ndarray:
-    # 初期化
-    pixel_list: list = []
-    mean_list: list = []
 
+def mosaic_image() -> np.ndarray:
     # 画像の読み込み
     img: np.ndarray
     height: int
@@ -61,16 +58,17 @@ def mosaic_image() -> np.ndarray:
     x, y = get_mosaic_size()
 
     # モノクロかカラーかを選ぶ
-    is_monochrome: bool = monochrome_or_color()
-    if is_monochrome:
-        # モノクロでモザイク化
-        img = monochrome(img, height, width, x, y, pixel_list, mean_list)
-    else:
-        # カラーでモザイク化
-        img = color(img, height, width, x, y, pixel_list, mean_list)
+    # is_monochrome: bool = monochrome_or_color()
+
+    # モザイク化
+    img = mosaic(img, height, width, x, y)
+
+    # if is_monochrome:
+    #     # モノクロ化
+    #     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     print(
-        "height({}) // x({}) = {} ... {}\nwidth({}) // y({}) = {} ...{}".format(
+        "height({}) // x({}) = {} ... {}\nwidth({}) // y({}) = {} ... {}".format(
             height, x, height // x, height % x, width, y, width // y, width % y
         )
     )
@@ -78,57 +76,21 @@ def mosaic_image() -> np.ndarray:
     return img
 
 
-def monochrome(
+def mosaic(
     img: np.ndarray,
     height: int,
     width: int,
     x: int,
     y: int,
-    pixel_list: list,
-    mean_list: list,
 ) -> np.ndarray:
-    # 割り切れる部分をモザイク化
-    for i in tqdm((range(height // x))):
-        for j in range((width // y)):
-            # 各画素を取得
-            pixel_list.append(img[i * y : i * y + y, j * x : j * x + x].reshape(-1))
-            # 各画素の平均値を計算
-            mean_list.append(np.mean(img[i * y : i * y + y, j * x : j * x + x]))
-            # 計算した平均値を画像に反映
-            img[i * y : i * y + y, j * x : j * x + x] = np.mean(
-                img[i * y : i * y + y, j * x : j * x + x]
-            )
-
-    return img
-
-
-def color(
-    img: np.ndarray,
-    height: int,
-    width: int,
-    x: int,
-    y: int,
-    pixel_list: list,
-    mean_list: list,
-) -> np.ndarray:
-    # 割り切れる部分をモザイク化
+    # モザイク化
     for i in tqdm((range(height // x))):
         for j in range((width // y)):
             for k in range(3):
-                # 各画素を取得
-                pixel_list.append(
-                    img[i * y : i * y + y, j * x : j * x + x, k].reshape(-1)
-                )
-                # 各画素の平均値を計算
-                mean_list.append(np.mean(img[i * y : i * y + y, j * x : j * x + x, k]))
-                # 計算した平均値を画像に反映
+                # 各画素の平均値を計算し、画像に反映
                 img[i * y : i * y + y, j * x : j * x + x, k] = np.mean(
                     img[i * y : i * y + y, j * x : j * x + x, k]
                 )
-
-                # リセット
-                pixel_list.clear()
-                mean_list.clear()
 
     return img
 
